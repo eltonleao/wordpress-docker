11083cd28cbb8752d4373a0a6a4d54628cf58611
eltonleao <3lton.leao@gmail.com>
2023-11-05 11:43:28 -0300

    .

--- src/meu-plugin.php
+++ src/meu-plugin.php
@@ -4,10 +4,10 @@
  * Plugin Name: Meu Plugin
  */
 
-function my_content_footer($content)
-{
-    if (is_single()) {
-        return $content . '<p>MUITO OBRIGADO PELA SUA VISITA!</p>';
-    }
-}
-add_filter('the_content', 'my_content_footer');
+// function my_content_footer($content)
+// {
+//     if (is_single()) {
+//         return $content . '<p>MUITO OBRIGADO PELA SUA VISITA!</p>';
+//     }
+// }
+// add_filter('the_content', 'my_content_footer');
